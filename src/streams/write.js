36f0deeeed27bc112bd96a686936079df246296d
d.luzko <d.luzko@godeltech.com>
2024-10-03 22:32:11 +0200

    feat: implement write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+import { createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const write = async () => {
-    // Write your code here 
-};
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+  
+  const writeStream = createWriteStream(filePath);
+  
+  const handleError = () => {
+    throw new Error("Operation failed");
+  }
+  
+  process.stdin
+    .on("error", handleError)
+    .pipe(writeStream)
+    .on("error", handleError)
+}
 
 await write();
