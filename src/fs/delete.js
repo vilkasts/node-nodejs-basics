ab0bc36de88d4a0a21e3583a22f7da22c080d62b
d.luzko <d.luzko@godeltech.com>
2024-10-03 16:52:57 +0200

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { existsSync, promises } from "fs";
+
 const remove = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = join(__dirname, "files", "fileToRemove.txt");
+  
+  if (!existsSync(filePath)) {
+    throw new Error("FS operation failed");
+  }
+  
+  try {
+    await promises.unlink(filePath);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
