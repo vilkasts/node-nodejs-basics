aafb25f62bf88bc532696d34cad469dcea01ae25
d.luzko <d.luzko@godeltech.com>
2024-10-04 19:59:43 +0200

    feat: implement decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createUnzip } from "zlib";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const compressedFilePath = join(__dirname, "files", "archive.gz");
+  const decompressedFilePath = join(__dirname, "files", "fileToCompress.txt");
+  
+  const readStream = createReadStream(compressedFilePath);
+  const writeStream = createWriteStream(decompressedFilePath);
+  const compressStream = createUnzip()
+  
+  const handleError = () => {
+    throw new Error("Operation failed");
+  }
+  
+  readStream
+    .on("error", handleError)
+    .pipe(compressStream)
+    .pipe(writeStream)
+    .on("error", handleError)
 };
 
 await decompress();
